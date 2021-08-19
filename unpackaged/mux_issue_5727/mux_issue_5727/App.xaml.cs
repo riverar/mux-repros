e899d2e9b76b634615b5d02ea640e0743bd29701
Rafael Rivera <rafael@withinwindows.com>
2021-08-19 11:58:38 -0700

    Add repro for microsoft/microsoft-ui-xaml#5727

---
+++ unpackaged/mux_issue_5727/mux_issue_5727/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Microsoft.UI.Xaml.Shapes;
+
+namespace mux_issue_5727
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            var shape = new Rectangle();
+            shape.Width = 20f;
+            shape.Height = 20f;
+
+            var canvas = new Canvas();
+            canvas.Children.Add(shape);
+
+            var render_target = new RenderTargetBitmap();
+            render_target.RenderAsync(canvas).GetResults();
+        }
+    }
+}
---
+++ unpackaged/mux_issue_5727/mux_issue_5727/Entrypoint.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace mux_issue_5727
+{
+    public static class Entrypoint
+    {
+        [DllImport("Microsoft.WindowsAppSDK.Bootstrap", EntryPoint = "MddBootstrapInitialize", ExactSpelling = true, PreserveSig = true)]
+        public static extern uint MddBootstrapInitialize(
+            uint majorMinorVersion,
+            [MarshalAs(UnmanagedType.LPWStr)]
+            string versionTag,
+            ulong packageVersion);
+
+        [GeneratedCodeAttribute("Microsoft.UI.Xaml.Markup.Compiler", " 0.0.0.0")]
+        [DebuggerNonUserCodeAttribute()]
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+
+            MddBootstrapInitialize(1 << 16 | 0, "experimental1", 0);
+
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+            Application.Start((p) => {
+                var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                new App();
+            });
+        }
+    }
+}
