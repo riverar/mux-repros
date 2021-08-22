bcff284c26a8b8e681d9c51bcf4aa415b7882de1
Rafael Rivera <rafael@withinwindows.com>
2021-08-22 10:33:33 -0700

    Add unpackaged scratch project

---
+++ unpackaged/scratch/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace scratch
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
---
+++ unpackaged/scratch/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace scratch
+{
+    public static class Program
+    {
+        private class NativeMethods
+        {
+            [DllImport("Microsoft.WindowsAppSDK.Bootstrap", EntryPoint = "MddBootstrapInitialize", ExactSpelling = true, PreserveSig = true)]
+            public static extern uint MddBootstrapInitialize(
+                uint majorMinorVersion,
+                [MarshalAs(UnmanagedType.LPWStr)]
+                string versionTag,
+                ulong packageVersion);
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var hr = NativeMethods.MddBootstrapInitialize(1 << 16 | 0, "experimental1", 0);
+            if (hr != 0)
+            {
+                Marshal.ThrowExceptionForHR(unchecked((int)hr));
+            }
+
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+            Application.Start((p) =>
+            {
+                var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                new App();
+            });
+        }
+    }
+}
