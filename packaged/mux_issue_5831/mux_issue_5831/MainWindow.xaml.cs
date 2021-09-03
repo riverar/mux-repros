49d4b5164916458cdc65da7f2cd2025fd276a37d
Rafael Rivera <rafael@withinwindows.com>
2021-09-03 14:03:35 -0700

    Add packaged repro for microsoft/microsoft-ui-xaml#5831

---
+++ packaged/mux_issue_5831/mux_issue_5831/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml;
+using WinRT.Interop;
+
+namespace mux_issue_5831
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = WindowNative.GetWindowHandle(this);
+            
+            // Both of these fail. Pick one.
+
+            // _ = await PrintManagerInterop.ShowPrintUIForWindowAsync(hwnd);
+            // _ = await PrintManager.ShowPrintUIAsync();
+        }
+    }
+}
