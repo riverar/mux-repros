6e561411528857e811d6fc682105b6b618c5a904
Rafael Rivera <rafael@withinwindows.com>
2021-09-02 23:19:48 -0700

    Add repro for microsoft/microsoft-ui-xaml#5831

---
+++ unpackaged/mux_issue_5831/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace mux_issue_5831
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            _window = new MainWindow();
+            _window.Title = "Unpackaged WinUI 3 application";
+            _window.Activate();
+        }
+
+        private Window _window;
+    }
+}
---
+++ unpackaged/mux_issue_5831/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using Windows.Graphics.Printing;
+using WinRT.Interop;
+
+namespace mux_issue_5831
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = WindowNative.GetWindowHandle(this);
+            _ = await PrintManagerInterop.ShowPrintUIForWindowAsync(hwnd);
+        }
+    }
+}
